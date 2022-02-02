beb79f9d24d2dfafad7a1cfea044c090c02cb4f2
Atharva Joshi <atharvaj2160@gmail.com>
2022-02-02 22:20:16 +0530

    Fixed Grid

--- src/partials/Skills.jsx
+++ src/partials/Skills.jsx
@@ -5,7 +5,7 @@ const Skills = ({ skills }) => {
     return (
         <div className="tech-stack">
             <h1 className="text-2xl md:text-5xl text-center font-extrabold">Here's my Tech Stack</h1>
-            <div className="grid md:grid-cols-4 sm:grid-cols-2 grid-cols-2 max-w-2xl mx-auto mt-8">
+            <div className="grid md:grid-cols-4 sm:grid-cols-2 max-w-2xl mx-auto mt-8">
                 {skills.map((skill) => {
                     return (
                         <SkillCard
