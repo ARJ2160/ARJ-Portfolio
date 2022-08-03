1ee623fb5b7e71f4c98a9c8b8feb6fc7f7cffe5b
Atharva <atharvaj2160@gmail.com>
2022-08-03 11:05:52 +0530

    Added social media buttons

--- src/App.jsx
+++ src/App.jsx
@@ -10,12 +10,23 @@ import Footer from './partials/Footer.jsx';
 import Projects from './partials/Projects.jsx';
 import Skills from './partials/Skills.jsx';
 import { data } from './assets/data';
-import sunSvg from './images/sun.svg';
+import sunSvg from './images/lightMode.svg';
+import moonSvg from './images/darkMode.svg';
 import useDarkMode from './hooks/useDarkMode.js';
+import {
+  FaFolder,
+  FaGithub,
+  FaRegEnvelope,
+  FaSpotify,
+  FaTwitter,
+} from 'react-icons/fa';
+import { SiLinkedin } from 'react-icons/si';
+import ARJResume from './assets/ARJ-Resume.pdf';
 
 const App = () => {
   const [colorTheme, setTheme] = useDarkMode();
   const { name, title, skills, projects, social } = data;
+  const { GitHub, LinkedIn, Twitter, Email, Spotify } = social;
   useEffect(() => {
     AOS.init({
       once: true,
@@ -25,7 +36,7 @@ const App = () => {
   return (
     <div className='min-h-screen py-10 px-3 sm:px-5 dark:bg-backgroundBlue dark:text-white bg-white text-black'>
       <div data-aos='fade-down' data-aos-duration='800'>
-        <Card name={name} title={title} social={social} />
+        <Card />
       </div>
       <div data-aos='fade-up' data-aos-duration='800' data-aos-delay='400'>
         <About />
@@ -33,7 +44,7 @@ const App = () => {
         <Projects projects={projects} />
         <Footer github={social.GitHub} />
       </div>
-      <div>
+      {colorTheme === 'light' ? (
         <img
           src={sunSvg}
           width={30}
@@ -42,7 +53,102 @@ const App = () => {
           className='cursor-pointer fixed top-5 right-5 select-none'
           onClick={() => setTheme(colorTheme)}
         />
+      ) : (
+        <img
+          src={moonSvg}
+          width={30}
+          height={30}
+          alt='Toggle theme'
+          className='cursor-pointer fixed top-5 right-5 select-none'
+          onClick={() => setTheme(colorTheme)}
+        />
+      )}
+      <div className='social-media md:fixed md:block bottom-10 left-10 hidden'>
+        <ul className="social-media-list flex flex-col">
+          <li>
+            <a
+              className='relative flex justify-center items-center text-xl m-1 p-1 sm:m-2 sm:p-2 dark:text-white dark:hover:bg-gray-800 text-black hover:bg-teal-500 rounded-full group'
+              href={GitHub}
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <FaGithub />
+              <span className='tooltip group-hover:scale-100'>GitHub</span>
+              <span className='sr-only'>Github</span>
+            </a>
+          </li>
+          <li>
+            <a
+              className='relative flex justify-center items-center text-xl m-1 p-1 sm:m-2 sm:p-2 text-blue-700 hover:bg-teal-500 rounded-full group'
+              href={LinkedIn}
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <SiLinkedin />
+              <span className='tooltip group-hover:scale-100'>LinkedIn</span>
+              <span className='sr-only'>LinkedIn</span>
+            </a>
+          </li>
+          <li>
+            <a
+              className='relative flex justify-center items-center text-xl m-1 p-1 sm:m-2 sm:p-2 text-teal-500 hover:bg-black rounded-full group'
+              href={Spotify}
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <FaSpotify />
+              <span className='tooltip group-hover:scale-100'>Spotify</span>
+              <span className='sr-only'>Spotify</span>
+            </a>
+          </li>
+          <li>
+            <a
+              className='text-blue-500 hover:bg-blue-500 hover:text-white relative flex justify-center items-center text-xl m-1 p-1 sm:m-2 sm:p-2 rounded-full group'
+              href={Twitter}
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <FaTwitter />
+              <span className='tooltip group-hover:scale-100'>Twitter</span>
+              <span className='sr-only'>Twitter</span>
+            </a>
+          </li>
+          <li>
+            <a
+              className='relative flex justify-center items-center text-xl m-1 p-1 sm:m-2 sm:p-2 hover:text-white text-teal-500 hover:bg-teal-500 rounded-full group'
+              href={'https://mail.google.com/mail/?view=cm&fs=1&to=' + Email}
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <FaRegEnvelope />
+              <span className='tooltip group-hover:scale-100'>Email</span>
+              <span className='sr-only'>Email</span>
+            </a>
+          </li>
+          <li>
+            <a
+              className='relative flex justify-center items-center text-xl m-1 p-1 sm:m-2 sm:p-2 text-teal-500 hover:bg-black rounded-full group'
+              href={ARJResume}
+              download='Professional Resume - Atharva Joshi.pdf'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <FaFolder />
+              <span className='tooltip group-hover:scale-100'>Resume</span>
+              <span className='sr-only'>Resume</span>
+            </a>
+          </li>
+        </ul>
       </div>
+      {/* <div className='email flex flex-col items-center relative right-2 bottom-48 z-10 transition-all rotate-90'>
+        <a
+          href={'https://mail.google.com/mail/?view=cm&fs=1&to=' + Email}
+          target='_blank'
+          className='email-link fixed'
+        >
+          atharvaj2160@gmail.com
+        </a>
+      </div> */}
     </div>
   );
 };
--- src/partials/Card.jsx
+++ src/partials/Card.jsx
@@ -1,28 +1,12 @@
-import {
-  FaFolder,
-  FaGithub,
-  FaRegEnvelope,
-  FaSpotify,
-  FaTwitter,
-} from 'react-icons/fa';
-
-import ARJResume from '../assets/ARJ-Resume.pdf';
 import React from 'react';
-import { SiLinkedin } from 'react-icons/si';
 import pic from '../images/Atharva_Joshi.webp';
 import Typewriter from 'typewriter-effect';
 import { data } from '../assets/data';
 
-// import profile from "../images/profile.png";
-console.log(data);
-const Card = ({
-  name,
-  title,
-  social: { GitHub, LinkedIn, Twitter, Email, Spotify },
-}) => {
+const Card = () => {
   return (
     <div className='w-full min-h-full dark:text-black h-screen flex justify-center flex-col items-center text-center'>
-      <img src={pic} className='w-28 h-28 my-7 rounded-full' alt='ARJ Image' />
+      <img src={pic} className='w-28 h-28 my-7 shadow-xl rounded-full' alt='ARJ Image' />
       <div className='text-2xl md:text-xl font-mono flex items-center justify-between text-center text-skyBlue'>
         <Typewriter
           onInit={(typewriter) => {
@@ -51,80 +35,6 @@ const Card = ({
       <p className='sm:w-2/4 w-4/5 pt-5 md:text-lg md:text-center text-left text-base text-skyBlue'>
         {data.hero.about_me}
       </p>
-      {/* <div className='flex flex-col justify-center max-w-xs mx-auto bg-white shadow-2xl rounded-xl p-5 mt-10'>
-        <div className='profile-pic'>
-          <img
-            className='w-32 mx-auto shadow-xl rounded-full'
-            width='100%'
-            height='auto'
-            src={pic}
-            alt='Profile face'
-          />
-        </div>
-        <div className='text-center mt-5'>
-          <p className='name text-xl sm:text-2xl font-semibold'>{name}</p>
-          <p className='text-xs sm:text-base pt-2 pb-4 px-5 w-auto inline-block border-b-2'>
-            {title}
-          </p>
-          <div className='flex align-center justify-center mt-4'>
-            <a
-              className='relative flex justify-center items-center text-xl m-1 p-1 sm:m-2 sm:p-2 text-blue-500 hover:bg-blue-500 rounded-full hover:text-white transition-colors duration-0 group'
-              href={Twitter}
-              target='_blank'
-              rel='noopener noreferrer'>
-              <FaTwitter />
-              <span className='tooltip group-hover:scale-100'>Twitter</span>
-              <span className='sr-only'>Twitter</span>
-            </a>
-            <a
-              className='relative flex justify-center items-center text-xl m-1 p-1 sm:m-2 sm:p-2 text-teal-500 hover:bg-teal-500 rounded-full hover:text-white transition-colors duration-300 group'
-              href={'https://mail.google.com/mail/?view=cm&fs=1&to=' + Email}
-              target='_blank'
-              rel='noopener noreferrer'>
-              <FaRegEnvelope />
-              <span className='tooltip group-hover:scale-100'>Email</span>
-              <span className='sr-only'>Email</span>
-            </a>
-            <a
-              className='relative flex justify-center items-center text-xl m-1 p-1 sm:m-2 sm:p-2 text-gray-800 hover:bg-gray-800 rounded-full hover:text-white transition-colors duration-300 group'
-              href={GitHub}
-              target='_blank'
-              rel='noopener noreferrer'>
-              <FaGithub />
-              <span className='tooltip group-hover:scale-100'>GitHub</span>
-              <span className='sr-only'>Github</span>
-            </a>
-            <a
-              className='relative flex justify-center items-center text-xl m-1 p-1 sm:m-2 sm:p-2 text-blue-700 hover:bg-blue-900 rounded-full hover:text-white transition-colors duration-300 group'
-              href={LinkedIn}
-              target='_blank'
-              rel='noopener noreferrer'>
-              <SiLinkedin />
-              <span className='tooltip group-hover:scale-100'>LinkedIn</span>
-              <span className='sr-only'>LinkedIn</span>
-            </a>
-            <a
-              className='relative flex justify-center items-center text-xl m-1 p-1 sm:m-2 sm:p-2 text-teal-500 hover:bg-black rounded-full hover:text-white transition-colors duration-300 group'
-              href={ARJResume}
-              download='Professional Resume - Atharva Joshi.pdf'
-              target='_blank'
-              rel='noopener noreferrer'>
-              <FaFolder />
-              <span className='tooltip group-hover:scale-100'>Resume</span>
-              <span className='sr-only'>Resume</span>
-            </a>
-            <a
-              className='relative flex justify-center items-center text-xl m-1 p-1 sm:m-2 sm:p-2 text-teal-500 hover:bg-black rounded-full hover:text-white transition-colors duration-300 group'
-              href={Spotify}
-              target='_blank'
-              rel='noopener noreferrer'>
-              <FaSpotify />
-              <span className='tooltip group-hover:scale-100'>Spotify</span>
-              <span className='sr-only'>Spotify</span>
-            </a>
-          </div>
-        </div>
-      </div> */}
     </div>
   );
 };
